04b6f053c1b321e763a268528fbfa6d24a5aae17
Cal Henderson <cal@iamcal.com>
2020-12-28 23:59:00 +0000

    some basic tests

--- tests/IPv4Test.php
+++ tests/IPv4Test.php
@@ -2,9 +2,28 @@
 
 	use PHPUnit\Framework\TestCase;
 
-	final class IPV4Test extends TestCase{
+	final class IPv4Test extends TestCase{
 
 		function testBasic(){
 
+			$this->check("1.2.3.4", "1.2.3.4", "Simple dotted quad");
+		}
+
+		function check($in, $expected, $name){
+			$this->assertEquals($this->to_canonical($in), $expected, $name);
+		}
+
+		function to_canonical($in){
+			$p = new iamcal\IPParser();
+			try {
+				$out = $p->parse($in);
+				if ($out['type'] == 'ipv4'){
+					return $out['canonical'];
+				}else{
+					return $out['type'].':'.$out['canonical'];
+				}
+			} catch (Exception $e) {
+				return "Exception";
+			}
 		}
 	}
---
+++ tests/IPv6Test.php
@@ -0,0 +1,37 @@
+<?php
+
+	use PHPUnit\Framework\TestCase;
+
+	final class IPv6Test extends TestCase{
+
+		function testBasic(){
+
+			$this->assertEquals($this->to_canonical("1:2:3:4:5:6:7:8"), "1:2:3:4:5:6:7:8", "Simple colon-hex");
+
+			$this->assertEquals($this->to_canonical("1:2::3:4"), "1:2:0:0:0:0:3:4", "Elided zeros in middle");
+			$this->assertEquals($this->to_canonical("::1"), "0:0:0:0:0:0:0:1", "Elided zeros at start");
+			$this->assertEquals($this->to_canonical("1::"), "1:0:0:0:0:0:0:0", "Elided zeros at end");
+			$this->assertEquals($this->to_canonical("::"), "0:0:0:0:0:0:0:0", "Elided zeros entirely");
+
+			$this->assertEquals($this->to_canonical("1:2:3:4:5:6:77.77.88.88"), "1:2:3:4:5:6:4d4d:5858", "Dotted quad allowed for final 2 segments");
+
+			$this->assertEquals($this->to_canonical("fe80::1.2.3.4"), "fe80:0:0:0:0:0:102:304", "Dotted quad combined with elided zeros");
+
+			$this->assertEquals($this->to_canonical("0001:0002:0003:0004:0005:0006:0007:0008"), "1:2:3:4:5:6:7:8", "Leading zero are OK");
+			$this->assertEquals($this->to_canonical("00001:0002:0003:0004:0005:0006:0007:0008"), "Exception", "But no more than 4 hex-digits per segment");
+		}
+
+		function to_canonical($in){
+			$p = new iamcal\IPParser();
+			try {
+				$out = $p->parse($in);
+				if ($out['type'] == 'ipv6'){
+					return $out['canonical'];
+				}else{
+					return $out['type'].':'.$out['canonical'];
+				}
+			} catch (Exception $e) {
+				return "Exception";
+			}
+		}
+	}
