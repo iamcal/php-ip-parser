959d18a131febe73e5e43127652352d997f83e6e
Cal Henderson <cal@iamcal.com>
2021-04-20 17:00:32 +0000

    added more test cases for IPv4 addresses

--- tests/IPv4Test.php
+++ tests/IPv4Test.php
@@ -7,6 +7,17 @@
 		function testBasic(){
 
 			$this->check("1.2.3.4", "1.2.3.4", "Simple dotted quad");
+
+			$this->check("3232271615", "192.168.140.255", "Single long");
+
+			$this->check("0300.0250.0214.0377", "192.168.140.255", "Octal quad");
+
+			$this->check("0xc0.0xa8.0x8c.0xff", "192.168.140.255", "Hex quad");
+
+			$this->check("192.168.36095", "192.168.140.255", "Class B address");
+			$this->check("192.11046143", "192.168.140.255", "Class A address");
+
+			$this->check("0300.0xa8.36095", "192.168.140.255", "Mix of styles");
 		}
 
 		function check($in, $expected, $name){
