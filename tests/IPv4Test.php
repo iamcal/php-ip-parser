8cacb6980e0b001560ae687d59d7f226af1f65b0
Cal Henderson <cal@iamcal.com>
2021-04-21 00:47:37 +0000

    a couple of overflow tests to make sure we're exercising all the codepaths

--- tests/IPv4Test.php
+++ tests/IPv4Test.php
@@ -7,6 +7,8 @@
 		function testBasic(){
 
 			$this->check("1.2.3.4", "1.2.3.4", "Simple dotted quad");
+
+			$this->check("1.2.3.257", "Exception", "Overflowing atom");
 		}
 
 		function testLong(){
@@ -21,6 +23,8 @@
 			$this->check("0xc0.0xa8.0x8c.0xff", "192.168.140.255", "Hex quad");
 
 			$this->check("0300.0xa8.36095", "192.168.140.255", "Mix of styles");
+
+			$this->check("0xc0.0xa8.0x8c.257", "Exception", "Overflowing atom");
 		}
 
 		function testClasses(){
