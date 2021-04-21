bf1b86445839fd4d9b3467b64c49a205cc4ed248
Cal Henderson <cal@iamcal.com>
2021-04-21 00:14:47 +0000

    break existing tests into more named groups

--- tests/IPv4Test.php
+++ tests/IPv4Test.php
@@ -7,17 +7,27 @@
 		function testBasic(){
 
 			$this->check("1.2.3.4", "1.2.3.4", "Simple dotted quad");
+		}
+
+		function testLong(){
 
 			$this->check("3232271615", "192.168.140.255", "Single long");
+		}
+
+		function testBases(){
 
 			$this->check("0300.0250.0214.0377", "192.168.140.255", "Octal quad");
 
 			$this->check("0xc0.0xa8.0x8c.0xff", "192.168.140.255", "Hex quad");
 
+			$this->check("0300.0xa8.36095", "192.168.140.255", "Mix of styles");
+		}
+
+		function testClasses(){
+
 			$this->check("192.168.36095", "192.168.140.255", "Class B address");
 			$this->check("192.11046143", "192.168.140.255", "Class A address");
 
-			$this->check("0300.0xa8.36095", "192.168.140.255", "Mix of styles");
 		}
 
 		function check($in, $expected, $name){
